b34f33bc884c4fc3de88d05846f47a086cb38cd1
Mark Hamilton <ham.mark126@gmail.com>
2022-10-12 18:58:34 -0500

    fixed c is for cookies code

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
 console.log("assignment")
+
+const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
+let y = 1
+
+for (let x = 0; x < cookies.length; x++) {
+    const currentCookie = cookies[x]
+    console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
+}
