6588a31bcf63e4a6f49ce6d70f9565d25bab9d50
Mark Hamilton <ham.mark126@gmail.com>
2022-10-12 19:58:23 -0500

    fixed codes on scope-exercises branch

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 console.log("assignment")
 
+// C is for Cookie
 const cookies = ["Oatmeal Raisin", "Chocolate Chip", "Sugar", "Peanut Butter", "Snickerdoodle", "Ginger"]
 let y = 1
 
@@ -8,13 +9,16 @@ for (let x = 0; x < cookies.length; x++) {
     console.log(`Mmmmmmm... that's a good ${currentCookie} cookie`)
 }
 
-function conjunction (firstWord, secondWord) {
+// Conjunction Function
+function conjunction(firstWord, secondWord) {
   const conjoinedWord = `${firstWord} ${secondWord}`
+  console.log(conjoinedWord)
 }
 
 conjunction("Master", "Card")
-console.log(conjoinedWord)
 
+
+// Mod Squad
   const modSquad = {
       "members": ["Pete Cochran", "Linc Hayes", "Julie Barnes", "Capt. Adam Greer", "Chief Barney Metcalf"],
       "series": {
@@ -30,3 +34,33 @@ console.log(conjoinedWord)
   })
 
 console.log(HTMLRepresentation)
+
+//Simon Says
+const locations = [[1,1], [1,2], [1,3], [2,1], [2,2], [2,3]]
+
+let invalidLocation = [];
+
+for (const location of locations) {
+
+    if (location[0] > 2) {
+    }
+
+    if (invalidLocation) {
+        console.log("This location is invalid")
+    }
+} 
+
+const llamaNamer = function () {
+  const possibleNames = ["Larry", "Leon", "Leona", "Les", "Laura", "Lemony", "Lars", "Lekisha"]
+  const randomizer = Math.floor(Math.random() * 7)
+
+  const namer = function () {
+      const suffix = " the Llama"
+      const name = possibleNames[randomizer]
+  }
+
+  return name + suffix
+}
+
+nameMaker = llamaNamer()
+console.log(nameMaker())
